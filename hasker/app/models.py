0978e1b988d8079312486d46e84067fd3dc2f75d
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-14 00:00:37 +0300

    User model added

--- hasker/app/models.py
+++ hasker/app/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
+class User(AbstractUser):
+    password = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to=user_directory_path)
+
