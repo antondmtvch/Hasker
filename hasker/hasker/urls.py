6c60bd6cffd3a68d95f9a997457e6d157a41ca92
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-16 00:27:56 +0300

    Updated main urls.py

--- hasker/hasker/urls.py
+++ hasker/hasker/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ask/', include('app.urls')),
+    path('', include('app.urls')),
 ]
 
 if settings.DEBUG:
