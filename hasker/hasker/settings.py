9d128f4ad2af298876ac96df9c19c2f0f79198d1
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-16 22:32:00 +0300

    Added STATIC_ROOT

--- hasker/hasker/settings.py
+++ hasker/hasker/settings.py
@@ -104,10 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -125,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'hasker/static'),
+]
 
 MEDIA_URL = '/media/'
 
