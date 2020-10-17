e1352a25c749ca8906e4671b85ee355a0a373753
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-17 23:07:57 +0300

    Added new question url

--- hasker/app/urls.py
+++ hasker/app/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', index_view, name='index'),
     path('question/<int:question_id>/', question_view, name='question'),
+    path('question/new/', ask_question_view, name='ask_question'),
 ]
