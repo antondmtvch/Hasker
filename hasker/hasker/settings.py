1abb86c2a9687e9cd223fa94cd97c00b6ef1b022
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-17 23:08:18 +0300

    Update settings

--- hasker/hasker/settings.py
+++ hasker/hasker/settings.py
@@ -108,13 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Moscow'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
@@ -127,5 +123,4 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
