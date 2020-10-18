445b1b59316768fbd12731cd28e7fd0561c03063
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-18 20:50:22 +0300

    Added clean_tags method on QuestionForm class

--- hasker/app/forms.py
+++ hasker/app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Question
 
 
@@ -6,3 +7,15 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ('title', 'text', 'tags')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'tags': forms.TextInput()
+        }
+
+    def clean_tags(self):
+        tags = self.cleaned_data['tags'].split(',')
+        tags = [tag.strip().lower() for tag in tags if tag.strip()]
+        if len(tags) > 3:
+            raise ValidationError('No more than 3 tags can be associated with the question')
+        return tags
