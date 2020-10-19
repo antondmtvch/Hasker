1b3a567b3d73b706fbe9cb9ac58fdd2ed8dc27ff
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-19 21:59:54 +0300

    Added CommaSeparatedField class for tags field on QuestionForm

--- hasker/app/forms.py
+++ hasker/app/forms.py
@@ -1,21 +1,36 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Question
+from .models import Question, Tag
+
+
+QUESTION_FORM_ATTRS = {
+    'class': 'form-control'
+}
+
+
+class CommaSeparatedField(forms.Field):
+    widget = forms.TextInput(attrs=QUESTION_FORM_ATTRS)
+
+    def to_python(self, value):
+        if value:
+            values = [i for i in map(str.strip, value.split(',')) if i]
+            if len(values) <= 3:
+                tags = []
+                for tag_name in values:
+                    tag = Tag.objects.get_or_create(name=tag_name)
+                    tags.append(tag)
+                return tags
+            else:
+                raise ValidationError('No more than 3 tags can be associated with the question')
 
 
 class QuestionForm(forms.ModelForm):
+    tags = CommaSeparatedField(required=False)
+
     class Meta:
         model = Question
         fields = ('title', 'text', 'tags')
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
-            'tags': forms.TextInput()
+            'title': forms.TextInput(attrs=QUESTION_FORM_ATTRS),
+            'text': forms.Textarea(attrs=QUESTION_FORM_ATTRS),
         }
-
-    def clean_tags(self):
-        tags = self.cleaned_data['tags'].split(',')
-        tags = [tag.strip().lower() for tag in tags if tag.strip()]
-        if len(tags) > 3:
-            raise ValidationError('No more than 3 tags can be associated with the question')
-        return tags
