bc96329135724d37e0a09b063823e5c4252c707a
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-20 21:59:49 +0300

    Updated to_python method on CommaSeparatedTextField class

--- hasker/app/forms.py
+++ hasker/app/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Question, Tag
-
+from .models import Question, Tag, Answer
 
 QUESTION_FORM_ATTRS = {
     'class': 'form-control'
@@ -13,16 +12,17 @@ class CommaSeparatedTextField(forms.Field):
     widget = forms.TextInput(attrs=QUESTION_FORM_ATTRS)
 
     def to_python(self, value):
+        tags = []
         if value:
             values = [i.lower() for i in map(str.strip, value.split(',')) if i]
             if len(values) <= 3:
-                tags = []
                 for tag_name in values:
                     tag, created = Tag.objects.get_or_create(name=tag_name)
                     tags.append(tag)
                 return tags
             else:
                 raise ValidationError('No more than 3 tags can be associated with the question')
+        return tags
 
 
 class QuestionForm(forms.ModelForm):
