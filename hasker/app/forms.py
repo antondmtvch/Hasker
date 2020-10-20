ee731eee7bbb978538e1e43247cf98901185fa09
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-20 23:27:33 +0300

    Added UserRegisterForm class

--- hasker/app/forms.py
+++ hasker/app/forms.py
@@ -1,11 +1,24 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
 
-from .models import Question, Tag, Answer
+from .models import Question, Tag, Answer, User
 
 QUESTION_FORM_ATTRS = {
     'class': 'form-control'
 }
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(max_length=128, label='Username', widget=forms.TextInput(attrs=QUESTION_FORM_ATTRS))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs=QUESTION_FORM_ATTRS))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs=QUESTION_FORM_ATTRS))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs=QUESTION_FORM_ATTRS))
+    avatar = forms.ImageField(label='Avatar', widget=forms.FileInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'avatar')
 
 
 class CommaSeparatedTextField(forms.Field):
--- hasker/app/urls.py
+++ hasker/app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import QuestionDetailView, IndexView, CreateQuestionView
+from .views import QuestionDetailView, IndexView, CreateQuestionView, UserRegisterView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('register/', UserRegisterView.as_view(), name='register'),
     path('question/<int:pk>/', QuestionDetailView.as_view(), name='question'),
     path('question/new/', CreateQuestionView.as_view(), name='create_question'),
 ]
