21bf45092bb55f1dc1ebfaf643256e6f83ac6437
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-20 22:01:45 +0300

    Added AnswerQuestionView

--- hasker/app/views.py
+++ hasker/app/views.py
@@ -2,11 +2,12 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Question
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 
 class IndexView(ListView):
     model = Question
+    paginate_by = 2
     template_name = 'app/index.html'
     context_object_name = 'questions'
     queryset = Question.objects.select_related('author')
@@ -25,3 +26,7 @@ class CreateQuestionView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class AnswerQuestionView(CreateView):
+    form_class = AnswerForm
