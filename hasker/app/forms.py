e8368f995812235f8d785f762041c68f3fb79561
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-24 22:41:30 +0300

    Added UserLoginForm

--- hasker/app/forms.py
+++ hasker/app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from .models import Question, Tag, Answer, User
 
@@ -14,11 +14,16 @@ class UserRegisterForm(UserCreationForm):
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs=QUESTION_FORM_ATTRS))
     password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs=QUESTION_FORM_ATTRS))
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs=QUESTION_FORM_ATTRS))
-    avatar = forms.ImageField(label='Avatar', widget=forms.FileInput())
+    avatar = forms.ImageField(label='Avatar', widget=forms.FileInput(), required=False)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', 'avatar')
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=128, label='Username', widget=forms.TextInput(attrs=QUESTION_FORM_ATTRS))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs=QUESTION_FORM_ATTRS))
 
 
 class CommaSeparatedTextField(forms.Field):
