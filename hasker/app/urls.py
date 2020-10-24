d155637d752c859ce3257a2c5127f4ae7adf5b5c
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-24 22:42:43 +0300

    Update urls.py

--- hasker/app/urls.py
+++ hasker/app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import QuestionDetailView, IndexView, CreateQuestionView, UserRegisterView
+from .views import QuestionDetailView, IndexView, CreateQuestionView, UserRegisterView, UserLoginView, UserLogoutView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
     path('question/<int:pk>/', QuestionDetailView.as_view(), name='question'),
     path('question/new/', CreateQuestionView.as_view(), name='create_question'),
 ]
