3d5d2a0be027462f7e3f1a9af08511400a97fcf2
Anton Teplyakov <antondmtvch@gmail.com>
2020-10-31 22:46:40 +0300

    Updated UserRegisterView

--- hasker/accounts/views.py
+++ hasker/accounts/views.py
@@ -1,14 +1,34 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import UpdateView
 
-from .forms import UserRegisterForm, UserLoginForm
 from .models import User
+from .forms import UserRegisterForm, UserLoginForm
 
 
-class UserRegisterView(CreateView):
-    model = User
+class UserRegisterView(View):
     form_class = UserRegisterForm
     template_name = 'accounts/register_user.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            new_user = form.save()
+            messages.info(request, 'Thanks for registering. You are now logged in.')
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'])
+            login(request, new_user)
+            return redirect(reverse_lazy('index'))
+        return render(request, self.template_name, {'form': form})
 
 
 class UserLoginView(LoginView):
